8bea75192c365a929ce0f03ed482f45ffc2ad29f
yzg011 <357239268@qq.com>
2026-08-07 23:10:34 +0800

    commit message

--- src/config/site.ts
+++ src/config/site.ts
@@ -109,7 +109,7 @@ export const navConfig: {
     { name: "友链", href: "/friends" },
     { name: "关于", href: "/about" },
     { name: "归档", href: "/posts" },
-    // { name: "统计", href: "/stats" },
+    { name: "统计", href: "/stats" },
     { name: "标签", href: "/tags" },
   ],
   /** 移动端汉堡菜单中的额外项 */
